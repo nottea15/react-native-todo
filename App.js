b48755959b38bf712d12c123c592c4d27a2cc1fc
nottea15 <th3fr3eze@gmail.com>
2022-09-11 20:40:02 +0300

    todo list

--- App.js
+++ App.js
@@ -1,11 +1,72 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import {
+  SafeAreaView,
+  StyleSheet,
+  View,
+  ScrollView,
+  Text,
+  KeyboardAvoidingView,
+  Platform,
+  TextInput,
+  TouchableOpacity,
+  Keyboard,
+} from "react-native";
+import Task from "./components/Task";
 
 export default function App() {
+  const [task, setTask] = useState();
+  const [taskItems, setTaskItems] = useState([]);
+
+  const handleAddTask = () => {
+    Keyboard.dismiss();
+    setTaskItems([...taskItems, task]);
+    setTask(null);
+  };
+
+  const completeTask = (index) => {
+    let itemsCopy = [...taskItems];
+    itemsCopy.splice(index, 1);
+    setTaskItems(itemsCopy);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ScrollView style={styles.wrapper}>
+        <Text style={styles.sectionTitle}>Tasks to do</Text>
+
+        <View style={styles.list}>
+          {taskItems.map((task, index) => {
+            return (
+              <TouchableOpacity
+                key={index}
+                onPress={() => {
+                  completeTask(index);
+                }}
+              >
+                <Task text={task} />
+              </TouchableOpacity>
+            );
+          })}
+        </View>
+      </ScrollView>
+
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={styles.actionWrapper}
+      >
+        <TextInput
+          style={styles.input}
+          placeholder={"Write a task"}
+          value={task}
+          onChangeText={(text) => setTask(text)}
+        />
+
+        <TouchableOpacity onPress={() => handleAddTask()} disabled={!task}>
+          <View style={styles.addWrapper}>
+            <Text styles={styles.addButton}>+</Text>
+          </View>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
     </View>
   );
 }
@@ -13,8 +74,48 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#ccc",
+  },
+  wrapper: {
+    paddingTop: 80,
+    paddingHorizontal: 20,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+  list: {
+    marginTop: 30,
+  },
+  actionWrapper: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+    position: "absolute",
+    bottom: 20,
+    width: "100%",
+  },
+  input: {
+    paddingVertical: 15,
+    paddingHorizontal: 15,
+    width: 250,
+    borderRadius: 60,
+    backgroundColor: "#fff",
+    borderColor: "#C0C0C0",
+    borderWidth: 1,
+  },
+  addWrapper: {
+    width: 60,
+    height: 60,
+    borderRadius: 52,
+    backgroundColor: "#FFF",
+    justifyContent: "center",
+    alignItems: "center",
+    borderColor: "#C0C0C0",
+    color: "C0C0C0",
+    borderWidth: 1,
+  },
+  addButton: {
+    fontSize: 25,
   },
 });
